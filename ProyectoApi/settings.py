1753b66383524f3feec8f25b4f5011bfac3fbf5c
Hollman Rojas <hollmanerojas@gmail.com>
2025-01-31 06:28:08 -0500

    Feature/Add configuration database

--- ProyectoApi/settings.py
+++ ProyectoApi/settings.py
@@ -87,8 +87,15 @@ WSGI_APPLICATION = 'ProyectoApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bdempresa',
+        'USER': 'root',
+        'PASSWORD': '80121042',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
